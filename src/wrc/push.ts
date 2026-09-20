939fd52039a8878408b33e9e8623a9c05ec4c5f7
wrxx011 <wrxxdev@proton.me>
2026-09-20 18:13:27 +0200

    improve inserts to db

--- src/wrc/push.ts
+++ src/wrc/push.ts
@@ -2,29 +2,33 @@ import { scrapeSchedule } from "./scrape";
 import { sql, SQL } from "bun";
 import { formatISO } from "date-fns";
 
-const TEMP_URL =
-  "https://www.wrc.com/en/events/wrc-secto-rally-finland-2026/itinerary-wrc-secto-rally-finland-2026";
-const TEMP_ID = "wrc-secto-rally-finland-2026";
+async function pushDataToDB(url: string) {
+  const eventRegex = /(?<=\/events.)\S*(?=\/)/gi;
+  const event_id = url.match(eventRegex) || [""];
 
-const schedule = await scrapeSchedule(TEMP_URL);
+  const schedule = await scrapeSchedule(url);
 
-schedule.days.forEach(async (day) => {
-  day.stages.forEach(async (stage) => {
-    const currDate = new Date();
-    const stageTimestamp =
-      currDate.getFullYear() +
-      " " +
-      day.date +
-      " " +
-      stage.stageStartTime +
-      " " +
-      schedule.timezone;
-    const parsedStageTimestamp = formatISO(Date.parse(stageTimestamp));
-    const ret = await sql`
+  schedule.days.forEach(async (day) => {
+    day.stages.forEach(async (stage) => {
+      const currDate = new Date();
+      const stageTimestamp =
+        currDate.getFullYear() +
+        " " +
+        day.date +
+        " " +
+        stage.stageStartTime +
+        " " +
+        schedule.timezone;
+      const parsedStageTimestamp = formatISO(Date.parse(stageTimestamp));
+      const ret = await sql`
     INSERT INTO sessions (session_type, session_name, scheduled_at, event_id, session_no)
-    VALUES ('special_stage', ${stage.stageName}, ${parsedStageTimestamp}, ${TEMP_ID}, ${stage.specialStageNumber})
+    VALUES ('special_stage', ${stage.stageName}, ${parsedStageTimestamp}, ${event_id}, ${stage.specialStageNumber})
+    ON CONFLICT (event_id, session_no) DO NOTHING
     RETURNING *
     `;
-    console.log(ret);
+      console.log(ret);
+    });
   });
-});
+}
+
+pushDataToDB(Bun.argv[2] || "");
